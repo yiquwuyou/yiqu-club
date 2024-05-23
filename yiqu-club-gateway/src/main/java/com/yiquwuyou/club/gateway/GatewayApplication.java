befa82fd68d5bcef2d4e9093e158cdc1017e7e76
yiquwuyou <13613958265@163.com>
2024-05-23 15:41:55 +0800

    oss、auth、subject服务注册到nacos，gateway配置微服务路由

---
+++ yiqu-club-gateway/src/main/java/com/yiquwuyou/club/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.yiquwuyou.club.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 网关启动器
+ * 
+ * @author: ChickenWing
+ * @date: 2023/10/11
+ */
+@SpringBootApplication
+@ComponentScan("com.yiquwuyou")
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+
+}
