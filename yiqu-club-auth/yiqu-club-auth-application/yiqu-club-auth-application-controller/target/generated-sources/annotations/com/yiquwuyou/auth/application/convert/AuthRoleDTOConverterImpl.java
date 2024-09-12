54d408eadd7e444147ec171ca99310fe030b2483
安宵艺 <13613958265@163.com>
2024-09-12 11:24:53 +0800

    这一块我也有点懵(大部分是自动更新了时间的)，反正是准备写mq前的

--- yiqu-club-auth/yiqu-club-auth-application/yiqu-club-auth-application-controller/target/generated-sources/annotations/com/yiquwuyou/auth/application/convert/AuthRoleDTOConverterImpl.java
+++ yiqu-club-auth/yiqu-club-auth-application/yiqu-club-auth-application-controller/target/generated-sources/annotations/com/yiquwuyou/auth/application/convert/AuthRoleDTOConverterImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-09T08:47:14+0800",
+    date = "2024-09-12T10:17:26+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_192 (Oracle Corporation)"
 )
 public class AuthRoleDTOConverterImpl implements AuthRoleDTOConverter {
--- yiqu-club-subject/yiqu-club-domain/target/generated-sources/annotations/com/yiquwuyou/subject/domain/convert/SubjectLikedBOConverterImpl.java
+++ yiqu-club-subject/yiqu-club-domain/target/generated-sources/annotations/com/yiquwuyou/subject/domain/convert/SubjectLikedBOConverterImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-09T23:36:52+0800",
+    date = "2024-09-12T10:17:46+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_192 (Oracle Corporation)"
 )
 public class SubjectLikedBOConverterImpl implements SubjectLikedBOConverter {
